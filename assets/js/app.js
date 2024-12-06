eeb09aa7d18dd744be99db34b87f788c0e11d2b9
shiran avizohar <shiran0204@gmail.com>
2024-12-06 14:08:11 +0200

    Add JS before fixing bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+
+
 function collectData() {
     const description = document.getElementById('description').value
     const date = document.getElementById('date').value
@@ -15,7 +17,7 @@ function generateHTML(data , show) {
         const newHTML = `
         <div class="task ${show}" data-index="${data.index}">
             <div class="close-icon">
-                <img src="assets/image/picX.png" onclick="deleteTask(${data.index})">
+                 <i class="bi bi-x" onclick="deleteTask(${data.index})"></i>
             </div>
             <div class="taskContent"> ${data.description}</div>
             <div class="taskFooter">
@@ -27,7 +29,6 @@ function generateHTML(data , show) {
     console.log(newHTML)
     return newHTML
 }
-
 
 
 function renderHTML(newHTML) {
@@ -42,7 +43,6 @@ function clearForm() {
     const descriptionInput = document.getElementById('description')
     descriptionInput.focus()
 }
-
 
 
 function saveTaskToStorage(taskObject) {
@@ -98,18 +98,17 @@ function addTask(event) {
     const newHTML = generateHTML(data, "");
     renderHTML(newHTML);
 
-    // מציאת המשימה החדשה
+  
     const taskElement = document.querySelector(`.task[data-index="${data.index}"]`);
     
-    // הוספת מחלקת 'show' אחרי שהמשימה הוצגה
+    
     setTimeout(() => {
         taskElement.classList.add('show');
-    }, 10); // מחכים 10ms כדי להפעיל את האנימציה אחרי שהדף נטען
+    }, 10); 
 
     saveTaskToStorage(data);
     clearForm();
 }
-
 
 // function deleteTask(index) {
 //     alert(`will delete item #${index} from local storage`)
@@ -120,34 +119,30 @@ function addTask(event) {
 //     loadTasksFromLocalStorage();
 // }
 
-
 function deleteTask(index) {
     alert(`will delete item #${index} from local storage`);
     const tasks = JSON.parse(localStorage.getItem('tasks'));
     tasks.splice(index, 1);
     localStorage.setItem('tasks', JSON.stringify(tasks));
 
-    // מצא את האלמנט עם ה- data-index המתאים וימחק אותו
     const taskElement = document.querySelector(`.task[data-index="${index}"]`);
-    taskElement.remove(); // מוחק את המשימה הספציפית מתוך הדף
+    taskElement.remove();
 }
 
 function renderTasks() {
-    // טוען את כל המשימות מ-localStorage
+ 
     const tasksContainer = document.getElementById('tasks');
-    tasksContainer.innerHTML = ''; // מנקה את התוכן הקיים
+    tasksContainer.innerHTML = ''; 
 
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-    // מציג את כל המשימות מחדש
     tasks.forEach(task => {
-        const newHTML = generateHTML(task); // יוצר את ה-HTML של כל משימה
-        tasksContainer.innerHTML += newHTML; // מוסיף את ה-HTML החדש
+        const newHTML = generateHTML(task); 
+        tasksContainer.innerHTML += newHTML; 
     });
 
     
 }
-
 
 
 initStorage()
